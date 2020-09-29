f30f7b4ea303afa2260bed69cc9576426f57284c
ravinduf <ravinduf@rocketmail.com>
2020-09-29 13:57:24 +0530

    added fetching jobs functionality

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -7,6 +7,8 @@ const  ACTIONS = {
     ERROR: 'error'
 }
 
+const BASE_URL = "https://jobs.github.com/positions.json"
+
 function reducer(state, action) {
     switch(ACTIONS.type) {
         case ACTIONS.MAKE_REQUEST:
@@ -28,13 +30,18 @@ const useFetchJobs = (params, page) => {
     
     useEffect(() => {
         dispatch({ type: ACTIONS.MAKE_REQUEST });
+        
+        axios.get(BASE_URL, {
+            params: { markdown: true, page: page, ...params }
+        }).then(res => {
+            dispatch({type: ACTIONS.GET_DATA, payload: { jobs: res.data }})
+        }).catch(e => {
+            dispatch({ type: ACTIONS.ERROR, payload: { error: e }})
+        });
+
     }, [params, page]);
 
-    return {
-        jobs: [],
-        loading: true,
-        error: false
-    }
+    return state;
 }
 
 export default useFetchJobs;
