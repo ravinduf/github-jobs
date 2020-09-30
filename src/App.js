a20c613322242a492ea9cca43715fe51b3615e1c
ravinduf <ravinduf@rocketmail.com>
2020-09-30 17:56:14 +0530

    created params and pagenumber states and rendered jobs component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
 
 function App() {
+
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
 
   const { jobs, loading, error } = useFetchJobs();
   
@@ -10,7 +13,9 @@ function App() {
     <Container>
       {loading && <h1>Loading</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
-      <h1>{jobs.length}</h1>
+      {jobs.map(job => (
+        < Job key={job.id} job={job} />
+      ))}
     </Container>  
   );
 }
