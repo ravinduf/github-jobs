41a873a1576fcc4954316e396573f86250f41dd4
ravinduf <ravinduf@rocketmail.com>
2020-10-05 09:33:25 +0530

    added collapse

--- src/Job.js
+++ src/Job.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Card, Badge } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, Badge, Button, Collapse } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
 
 function Job({job}) {
+    const [open, setOpen] = useState(false);
+
     return (
         <Card>
             <Card.Body>
@@ -22,6 +24,17 @@ function Job({job}) {
                     </div>
                     <img className="d-none d-md-block" height="50" alt={job.company} src={job.company_logo} />
                 </div>
+                <Card.Text>
+                    <Button 
+                    onClick={() => setOpen(prevOpen => !prevOpen)} 
+                    variant="primary">View Details</Button>
+                    
+                </Card.Text>
+                <Collapse in={open} >
+                    <div className="mt-4">
+                        <ReactMarkdown source={job.description} />
+                    </div>
+                </Collapse>
             </Card.Body>
         </Card>
     )
