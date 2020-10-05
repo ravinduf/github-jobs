56381c07d90d7ca3bc1c5273c61d3f32a0d6fd16
ravinduf <ravinduf@rocketmail.com>
2020-10-05 15:33:44 +0530

    updated

--- src/JobsPagination.js
+++ src/JobsPagination.js
@@ -4,9 +4,12 @@ import { Pagination } from 'react-bootstrap'
 const JobsPagination = ( {page, setPage } ) => {
     return (
         <Pagination>
-            <Pagination.Prev/>
-            <Pagination.Item>{page}</Pagination.Item>
-            <Pagination.Item>{page}</Pagination.Item>
+            {page !== 1 && <Pagination.Prev/>}
+            {page > 2 && <Pagination.Item>1</Pagination.Item>}
+            {page > 3 && <Pagination.Ellipsis />}
+            {page !== 1 && <Pagination.Item>{page - 1}</Pagination.Item> }
+            <Pagination.Item active>{page}</Pagination.Item>
+            <Pagination.Item>{page + 1}</Pagination.Item>
             <Pagination.Next/>
         </Pagination>
 
