3b1f5581a08f2664356704a9814583e6b10e5663
ravinduf <ravinduf@rocketmail.com>
2020-10-05 21:05:38 +0530

    added location column

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -12,11 +12,11 @@ const SearchForm = ({ params, onParamsChange }) => {
                     name="description" type="text" />
                 </Form.Group>
 
-                {/* <Form.Group as={Col} >
+                <Form.Group as={Col} >
                     <Form.Label>Location</Form.Label>
                     <Form.Control onChange={onParamChange} value={params.location}
                     name="location" type="text" />
-                </Form.Group> */}
+                </Form.Group>
 
             </Form.Row>
         </Form >
