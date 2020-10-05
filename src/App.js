0ede5b10cf0e9707f555c0db6b22447a88c3527f
ravinduf <ravinduf@rocketmail.com>
2020-10-05 18:09:06 +0530

    rendered searchform

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap';
 
 import Job from './Job';
 import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
 
 function App() {
 
@@ -12,9 +13,20 @@ function App() {
 
   const { jobs, loading, error, hasNextPage } = useFetchJobs( params, page );
   
+  const handleParamChange = (e) => {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams(prevParams => {
+      return{ ...prevParams, [param]: value };
+
+    })
+  }
+
   return (
     <Container className="my-4">
       <h1 className="mb-4">GitHub Jobs</h1>
+      <SearchForm params={params} onParamsChange={handleParamChange}/>
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
       {loading && <h1>Loading</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
