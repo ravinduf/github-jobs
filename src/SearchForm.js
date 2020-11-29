50cc3cd0b2c3b4ebcc36b717869549a827476191
ravinduf <ravinduf@rocketmail.com>
2020-11-29 11:29:42 +0530

    improved msg showing
    need to improve it more

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -3,23 +3,25 @@ import { Form, Col } from 'react-bootstrap'
 
 const SearchForm = ({ params, onParamsChange }) => {
     return (
-        <Form className="mb-4">
-            <Form.Row>
-                
-                <Form.Group as={Col} >
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control onChange={onParamsChange} value={params.description}
-                    name="description" type="text" />
-                </Form.Group>
+        <div>
+            <Form className="m-3">
+                <Form.Row>
+                    
+                    <Form.Group as={Col} >
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control onChange={onParamsChange} value={params.description}
+                        name="description" type="text" />
+                    </Form.Group>
 
-                <Form.Group as={Col} >
-                    <Form.Label>Location</Form.Label>
-                    <Form.Control onChange={onParamsChange} value={params.location}
-                    name="location" type="text" />
-                </Form.Group>
+                    <Form.Group as={Col} >
+                        <Form.Label>Location</Form.Label>
+                        <Form.Control onChange={onParamsChange} value={params.location}
+                        name="location" type="text" />
+                    </Form.Group>
 
-            </Form.Row>
-        </Form >
+                </Form.Row>
+            </Form >
+        </div>
     )
 }
 
